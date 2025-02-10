0f653fe26be5630ba2cf09741fb658e325123aba
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-02-10 20:16:57 +0530

    graph

---
+++ bar.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+yrs=[2011,2010,2024,2016,2013]
+sales=[45,12,35,56,100]
+plt.bar(yrs,sales)
+plt.xlabel('years')
+plt.ylabel('sales')
+plt.show()
---
+++ g2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+yrs=[2010,2011,2012,2013,2014,2015]
+salse=[20,10,24,25,78,42]
+plt.plot(yrs,salse,marker="4")
+plt.xlabel('years')
+plt.ylabel('sales')
+plt.show()
