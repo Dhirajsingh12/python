39f53986de8b533372cfcc222b68ad4a2b5094a6
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-15 14:05:51 +0530

    conditaniol statement

---
+++ 10_problem.py
@@ -0,0 +1,9 @@
+a=int(input("enter your age"))
+if(a>=18):
+    print("you are able to vote")
+    
+elif(a<0):
+    print("please valid age")
+else:
+    print("you are not able to vote")
+print("end of the program")
