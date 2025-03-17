7fd55397c0cbc4be13ff88c093518463c26ca809
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-17 23:10:34 +0530

    function

---
+++ 23_problem.py
@@ -0,0 +1,8 @@
+def avg():
+    a=int(input("enter the first number="))
+    b=int(input("enter the second number="))
+    c=int(input("enter the third number="))
+    average=(a+b+c)/3
+    print(int(average))
+avg()
+    
