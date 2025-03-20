af46efa0de0bfcef9336152122d8f1df493fc43d
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-03-20 15:36:27 +0530

    fabonasi

---
+++ fabonasi.py
@@ -0,0 +1,16 @@
+n=int(input("enter the number="))
+a=0
+b=1
+count=0
+if(n<0):
+    print("enter the posistive number")
+elif(n==1):
+    print(n)
+else:
+    for i in range(n):
+        print(a)
+        c=a+b
+        a=b
+        b=c
+        
+    
